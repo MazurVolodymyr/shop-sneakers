5b14f0f26c5d700fa50bf4f9125398fa9984e813
Volodymyr <well.dream.on@gmail.com>
2023-06-23 15:43:32 +0300

    this project is almost ready, but I'm not interested in doing it

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -8,28 +8,32 @@ const Overlay = ( {doCartClose, items = [], onRemove }) => { //робимо в {
                 <h2>Кошик
                 <img src="./images/icons/btn-remove.svg" alt="remove" className='btn-remove-h2' onClick={doCartClose}/>
                 </h2>
-            
-                <div className="items">
-                {items.map(obj => <div className="cartItem">
-                        <img src={obj.imgURL} alt="" className='cartItem__sneakers'/>
-                        <div>
-                            <p>{obj.name}</p>
-                            <b>{obj.price}</b>
-                        </div>
-                        <img src="./images/icons/btn-remove.svg" alt="remove" className='btn-remove' onClick={()=> onRemove(obj.id)}/>
-                    </div>)}
-                </div>
+                
+                {items.length > 0 ? 
+                <div>
+                    <div className="items">
+                        {items.map(obj => <div className="cartItem">
+                            <img src={obj.imgURL} alt="" className='cartItem__sneakers'/>
+                            <div>
+                                <p>{obj.name}</p>
+                                <b>{obj.price}</b>
+                            </div>
+                            <img src="./images/icons/btn-remove.svg" alt="remove" className='btn-remove' onClick={()=> onRemove(obj.id)}/>
+                        </div>)}
+                    </div>
 
-                <div className='cartTotalBlock'>
-                    <ul >
-                        <li >
-                            <span>Разом</span>
-                            <div></div>
-                            <b> 15020</b>  
-                        </li>
-                    </ul>
-                    <button className='greenButton'>Замовити <img src="./images/icons/arrow.svg" alt="arrow" /></button>
-                </div>
+                    <div className='cartTotalBlock'>
+                        <ul >
+                            <li >
+                                <span>Разом</span>
+                                <div></div>
+                                <b> 15020</b>  
+                            </li>
+                        </ul>
+                        <button className='greenButton'>Замовити <img src="./images/icons/arrow.svg" alt="arrow" /></button>
+                    </div>
+                </div> : <div>Корзина порожня</div> }
+                
             </div>
         </div>  
     )
